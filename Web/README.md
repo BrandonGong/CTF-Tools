8db2c5702a6b2c1a285987ab2fdf1b57d53f5c56
BrandonGong <BrandonGong@users.noreply.github.com>
2022-06-18 13:41:58 -0600

    Fixed issues in dirscan. Updated documentation.

--- Web/README.md
+++ Web/README.md
@@ -1,5 +1,5 @@
 # Port Scanner
-
+## Usage
 Simple scanner to check if ports are open on a machine. 
 ```
 usage: portscan.py [-h] [-p PORT] [-v] [-t TIMEOUT] [-m MAX_THREADS] ip
@@ -19,7 +19,7 @@ optional arguments:
                         Maximum thread count to use when scanning ports.
 ```
 
-Example
+## Example
 ```
 $ ./portscan.py 127.0.0.1
 Starting scan of 127.0.0.1...
@@ -27,4 +27,41 @@ PORT    STATE
 22      OPEN
 80      OPEN
 443     OPEN
+```
+
+# Directory Scanner
+## Usage
+```
+usage: dirscan.py [-h] [-w WORDLIST] [-s INCLUDE_STATUS] [-ns EXCLUDE_STATUS] [-x EXTENSIONS] [-m MAX_THREADS] [-v] url
+
+URL directory scanner. Example: python3 dirscan.py -w common.txt http://localhost:8000/MyWebsite/
+
+positional arguments:
+  url                   Base URL to scan.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -w WORDLIST, --wordlist WORDLIST
+                        Wordlist of paths to try
+  -s INCLUDE_STATUS, --include_status INCLUDE_STATUS
+                        Comma list of HTTP status codes to include
+  -ns EXCLUDE_STATUS, --exclude_status EXCLUDE_STATUS
+                        Comma list of status codes to exclude
+  -m MAX_THREADS, --max_threads MAX_THREADS
+                        Maximum thread count to use when scanning.
+  -v, --verbosity       Increase verbosity of the scanned port messages.
+```
+## Example
+```
+$ ./dirscan.py -w common.txt http://localhost:8000/MyWebsite/
+************************************
+URL:            http://localhost:8000/MyWebsite/
+Status Codes:   [200, 201, 202, 203, 301, 302, 307, 308, 401, 403]
+Word List:      common.txt
+Count:          4712
+************************************
+STATUS          PATH
+------------------------------------
+200             /css
+200             /js
 ```
--- Web/dirscan.py
+++ Web/dirscan.py
@@ -7,20 +7,42 @@ import queue
 from urllib.parse import urlparse
 
 def main(args):
+    # Test Connection
+    try:
+        requests.get(url=args.url)
+    except:
+        print(f"Error connecting to url: {args.url}")
+        return
+
     global status_codes
     status_codes = [int(s.strip()) for s in args.include_status.split(',')]
-    print(status_codes)
+    status_codes_exclude = [int(s.strip()) for s in args.exclude_status.split(',')]
+    for c in status_codes_exclude:
+        if c in status_codes:
+            status_codes.remove(c)
     
     #extensions = [''] + [(e.strip() for e in args.extensions.split(','))]
+
     # Get wordlist
     wordlist = open(args.wordlist,'r')
     global work_queue
     work_queue = queue.Queue()
+    path_count = 0
     for path in wordlist:
-        url = '/'.join(p.strip('/') for p in [args.url,path])
+        path_count += 1
+        url = '/'.join(p.strip('/') for p in [args.url,path.strip('\n')])
         work_queue.put(url)
 
-    print("STATUS\tPATH")
+    # Print header
+    print("************************************")
+    print(f"URL:\t\t{args.url}")
+    print(f"Status Codes:\t{status_codes}")
+    print(f"Word List:\t{args.wordlist}")
+    print(f"Count:\t\t{path_count}")
+    print("************************************")
+    print("STATUS\t\tPATH")
+    print("------------------------------------")
+    
     for _ in range(0,args.max_threads):
         t = threading.Thread(target=worker,daemon=True)
         t.start()
@@ -35,20 +57,22 @@ def worker():
         work_queue.task_done()
 
 def checkUrl(url,extension):
-
     if extension != '':
         url += '.' + extension
 
-    r = requests.get(url=url)
-    if r.status_code in status_codes:
-        path = urlparse(url).path
-        print(f"{r.status_code}\t{path}")
+    try:
+        r = requests.get(url=url)
+        if r.status_code in status_codes:
+            path = urlparse(url).path
+            print(f"{r.status_code}\t\t{path}")
+    except:
+        pass
 
 
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='URL directory scanner. Expample: python3 dirscan.py -w common.txt -x "html,js,php" http://localhost:8000/MyWebsite/')
+    parser = argparse.ArgumentParser(description='URL directory scanner. Example: python3 dirscan.py -w common.txt http://localhost:8000/MyWebsite/')
     parser.add_argument('url',
                         help='Base URL to scan.')
     parser.add_argument('-w','--wordlist',
@@ -58,9 +82,8 @@ if __name__ == '__main__':
                         help='Comma list of HTTP status codes to include')
     parser.add_argument('-ns','--exclude_status',
                         help='Comma list of status codes to exclude')
-    parser.add_argument('-x','--extensions',
-                        help='Comma list of extensions to try.')
-    parser.add_argument('-m','--max_threads',default=100,type=int,
+    # parser.add_argument('-x','--extensions', help='Comma list of extensions to try.') #TODO: add support for this
+    parser.add_argument('-m','--max_threads',default=25,type=int,
                         help='Maximum thread count to use when scanning.') 
     parser.add_argument('-v','--verbosity',action='count',default=0,
                         help='Increase verbosity of the scanned port messages.')                   
